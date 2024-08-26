76726c954fdb8c657b6a7c8e30b878e06820e7f4
Florencia Artegoytia <florencia@xperiencops.com>
2024-08-26 19:32:42 -0300

    endpoint para subir pdf

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,18 +1,36 @@
-import express, { Express } from 'express';
+import express, { Express, Request, Response } from 'express';
 
+import { leerPDF, upload } from './controllers/leerPDF.controller';
 import { main } from './lib/getPrevsFromCSV';
 import prevRoutes from './routes/prevs.routes';
 
-// const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT ?? 8080;
 
-// const app: Express = express();
+const app: Express = express();
 
-// app.use(express.json());
+app.use(express.json());
 
-// app.use('/api/prevs', prevRoutes);
+app.use('/api/prevs', prevRoutes);
 
-// app.listen(PORT, async () => {
-//   console.log(`Server is running at http://localhost:${PORT}`);
-// });
+app.post('/upload', upload.single('pdf'), (req: Request, res: Response) => {
+  if (!req.file) {
+    return res.status(400).send('No se subió ningún archivo');
+  }
 
-main();
+  // Usar filePath donde se guardo el archivo
+  const filePath = req.file.path;
+  leerPDF(filePath, res);
+
+  // Eliminar el archivo después de procesarlo
+  // fs.unlink(filePath, err => {
+  //   if (err) {
+  //     console.error(`Error al eliminar el archivo temporal: ${err}`);
+  //   }
+  // });
+});
+
+app.listen(PORT, () => {
+  console.log(`Servidor corriendo en http://localhost:${PORT}`);
+});
+
+//main();
