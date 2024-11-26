1a154cd6633bf22cec1e7cade9e753abdf6da5e4
Mathi Ramilo <mathiramilo2290@gmail.com>
2024-11-25 22:29:24 -0300

    chore: instalar tailwind y componentes base de shadcn

--- frontend/src/components/ui/button/Button.tsx
+++
@@ -1,9 +0,0 @@
-import { ComponentPropsWithoutRef } from 'react';
-
-import styles from './Button.module.css';
-
-type ButtonProps = ComponentPropsWithoutRef<'button'>;
-
-export const Button = ({ className, ...props }: ButtonProps) => (
-  <button className={`${styles.button} ${className}`} {...props} />
-);
--- frontend/src/components/ui/index.ts
+++ frontend/src/components/ui/index.ts
@@ -1,1 +1,4 @@
-export * from './button';
+export * from './Button';
+export * from './Checkbox';
+export * from './Dialog';
+export * from './Switch';
